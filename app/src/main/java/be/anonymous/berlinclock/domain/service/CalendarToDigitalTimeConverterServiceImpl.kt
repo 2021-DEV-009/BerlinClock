e776471a1429b3f293a7f4b8805e5938cae8f8a0
Anonymous <>
2021-05-08 11:00:28 +0200

    Add service to convert a Calendar instance to a DigitalTime instance

---
+++ app/src/main/java/be/anonymous/berlinclock/domain/service/CalendarToDigitalTimeConverterServiceImpl.kt
@@ -0,0 +1,16 @@
+package be.anonymous.berlinclock.domain.service
+
+import be.anonymous.berlinclock.domain.data.DigitalTime
+import java.util.*
+
+class CalendarToDigitalTimeConverterServiceImpl {
+
+    fun convert(calendar: Calendar): DigitalTime {
+        return DigitalTime(
+                hours = calendar.get(Calendar.HOUR_OF_DAY),
+                minutes = calendar.get(Calendar.MINUTE),
+                seconds = calendar.get(Calendar.SECOND)
+        )
+    }
+
+}
